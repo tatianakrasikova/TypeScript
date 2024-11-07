e958e74bb43a1cdb5e1d708bf001d50c0c3f9c83
tatianakrasikova <krasikovatatana535@gmail.com>
2024-11-07 20:33:50 +0100

    commit3

---
+++ src/homeworks/homework03/Homework03.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "../../components/loginForm/LoginForm";
+
+function Homework03() {
+  return (
+    <div className="lesson-container">
+      <h2>Homework 03</h2>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Homework03;
